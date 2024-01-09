c3157ad0612df0d23a6442e4fe8b01862ca4f26a
Elvya <154899158+Elvya@users.noreply.github.com>
2024-01-09 17:24:25 +0700

    new calculator

--- kalkulator/kalkulator.py
+++ kalkulator/kalkulator.py
@@ -2,9 +2,9 @@ print('---KALKULATOR---')
 print('----------------')
 print('')
 
-angka_1 = int(input('masukkan angka 1 :'))
+angka_1 = int(input('masukkan angka pertama :'))
 option = input('pilih opsi (+, -, *, /): ')
-angka_2 = int(input('masukkan angka 2 : '))
+angka_2 = int(input('masukkan angka kedua : '))
 
 #opsi
 opsi_plus = angka_1 + angka_2
@@ -14,12 +14,12 @@ opsi_bagi = angka_1 / angka_2
 print('--------------------------')
 
 if option == '+':
-    print('Hasil adalah : ' + str(opsi_plus))
+    print('Hasilnya adalah : ' + str(opsi_plus))
 elif option == '-':
-    print('Hasil adalah : ' + str(opsi_min))
+    print('Hasilnya adalah : ' + str(opsi_min))
 elif option == '*':
-    print('Hasil adalah : ' + str(opsi_kali))
+    print('Hasilhasilnya adalah : ' + str(opsi_kali))
 elif option == '/':
-    print('Hasil adalah : ' + str(opsi_bagi))
+    print('Hasilnya adalah : ' + str(opsi_bagi))
     print('dan jika dibulatkan, menjadi' + str(round(opsi_bagi)))
 print('--------------------')
